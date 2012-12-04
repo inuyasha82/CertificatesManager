e89d4759a9784981a6b32282ec62a014ba85eb32
Ivan Gualandri <ivan.gualandri@gmail.com>
2012-12-04 10:08:29 +0100

    TableItemListeners: now the handler read the data from the selected column

--- src/main/java/org/certificatesmanager/listeners/TableItemListener.java
+++ src/main/java/org/certificatesmanager/listeners/TableItemListener.java
@@ -3,19 +3,28 @@ package org.certificatesmanager.listeners;
 import org.certificatesmanager.Components;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableItem;
 
 public class TableItemListener implements Listener {
 	
 	private Components components;
+	private Table curTable;
 
 	public TableItemListener(Components components) {
 		this.components = components;
+		this.curTable = components.getAliasTable();
 	}
 
 	@Override
 	public void handleEvent(Event arg0) {
-		// TODO Auto-generated method stub
-		
+		TableItem[] item = curTable.getSelection();
+		int numColumn = curTable.getColumnCount();
+		System.out.println(item.length);				
+		for(int i=0; i<numColumn; i++){ 
+			System.out.print(item[0].getText(i) + " ");
+		}
+		System.out.println();
 	}
 
 }
